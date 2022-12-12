7b08ba057225a5e610a5431612ffd206e7779a10
ruanazevedo <ruann.azevedo@outlook.com>
2022-12-12 16:22:33 -0300

    exercicios beecrowd em csharp

---
+++ Exercicios/ExameGeral/ExameGeral/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExameGeral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int q;
+
+            string[] nums = Console.ReadLine().Split(' ');
+            n = int.Parse(nums[0]);
+            q = int.Parse(nums[1]);
+
+            List<int> notas = new List<int>();
+            notas.Add(30000);
+            
+            for (int i = 0; i < n; i++)
+            {
+                int nota = int.Parse(Console.ReadLine());
+                notas.Add(nota);
+            }
+
+            notas.Sort();
+            notas.Reverse();
+
+            List<int> consultadas = new List<int>();
+            for (int i = 0; i < q; i++)
+            {
+                int index = int.Parse(Console.ReadLine());
+                consultadas.Add(notas[index]);
+            }
+
+            foreach(int nota in consultadas)
+            {
+                Console.WriteLine(nota);
+            }
+        }
+    }
+}
---
+++ Exercicios/ListaDeChamadas/ListaDeChamadas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaDeChamadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+            int n = int.Parse(values[0]);
+            int k = int.Parse(values[1]);
+            List<string> nomes = new List<string>();
+
+            for (int i = 0; i < n; i++)
+                nomes.Add(Console.ReadLine());
+
+            nomes.Sort();
+            Console.WriteLine(nomes[k - 1]);
+        }
+    }
+}
